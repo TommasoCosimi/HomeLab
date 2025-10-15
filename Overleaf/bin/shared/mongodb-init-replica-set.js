e4aeaa78d6802352c70adc47b46e10b245fc6506
TommasoCosimi <tommaso.cosimi@protonmail.com>
2025-10-15 17:55:07 +0200

    Added Overleaf setup

---
+++ Overleaf/bin/shared/mongodb-init-replica-set.js
@@ -0,0 +1,5 @@
+/* Taken from the official Overleaf Repository (https://github.com/overleaf/overleaf) and adapted to the compose file */
+
+/* eslint-disable no-undef */
+
+rs.initiate({ _id: 'overleaf', members: [{ _id: 0, host: 'overleaf_mongo:27017' }] })
